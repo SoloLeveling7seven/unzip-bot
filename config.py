286c70b2e707889556b4672aaef1ddd7df13a744
Solo Leveling <109235836+SoloLeveling7seven@users.noreply.github.com>
2023-09-22 20:07:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ import os
 class Config:
     APP_ID = int(os.environ.get("APP_ID", "20263428"))
     API_HASH = os.environ.get("API_HASH", "f9dc42564f6e6eb9920912b09f729e3f")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6595643696:AAFz7QsIEhvZ1zV3ZPmdYN5MvBGI5TLC0T4")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL",-1001672535959))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6222983478:AAHcISRe4SnSFWlxtinm4m88nuKUhHRkbKo")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL",-1001973804769))
     MONGODB_URL = os.environ.get("MONGODB_URL","mongodb+srv://Luciferbin:9339119160@cluster0.kmbiojj.mongodb.net/?retryWrites=true&w=majority")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", "5437374877"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "25004026"))
     SESSION_STRING = os.environ.get("SESSION_STRING") if os.environ.get("SESSION_STRING") else None
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
